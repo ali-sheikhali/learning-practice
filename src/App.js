ac1916bca1ff4168659d1892497a160d52f2229f
ali-sheikhali <ali.sheikhali1998@gmail.com>
2024-02-05 12:02:27 +0330

    add file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react'
-import "slick-carousel/slick/slick.css"; 
+import React from "react";
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div>
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path='/' Component={Home} />
-            </Routes>
-        </Router>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" Component={Home} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,7 +22,7 @@ function Home() {
       <JoinUs />
       <Become />
       <ClubPoint />
-      <Footer />
+     
     </div>
   );
 }
